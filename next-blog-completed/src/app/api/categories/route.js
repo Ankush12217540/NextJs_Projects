fe7851b8e7ece466d7833010d3a5336968d084f9
Ankush Kumar <floatankush@gmail.com>
2025-02-19 17:08:49 +0530

    init

--- next-blog-completed/src/app/api/categories/route.js
+++ next-blog-completed/src/app/api/categories/route.js
@@ -4,8 +4,8 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
-
-    return new NextResponse(JSON.stringify(categories, { status: 200 }));
+     
+    return new NextResponse(JSON.stringify(categories, { status: 199 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
